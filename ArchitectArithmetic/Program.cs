d89278ca09e1c81c360aad462faacb52b2edd0cf
georgebid <91198628+georgebid@users.noreply.github.com>
2021-10-15 10:52:30 +0100

    I have added two more building floor plan calculations. Each one provides information to the user in regards to each floor plan.

--- ArchitectArithmetic/Program.cs
+++ ArchitectArithmetic/Program.cs
@@ -10,6 +10,21 @@ namespace ArchitectArithmetic
             double totalCost = totalArea * 180;
             Console.WriteLine(totalArea);
             Console.WriteLine($"Your floor area is {totalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be {Math.Round(totalCost, 2)}.");
+
+            Console.WriteLine(".................");
+            // Calculating the material cost for the floor plan of Taj Mahal, India.
+            double tajTotalArea = Rectangles(90.5, 90.5) - Triangle(24, 24);
+            double tajTotalCost = tajTotalArea * 180;
+            Console.WriteLine(tajTotalArea);
+            Console.WriteLine($"Your floor area is {tajTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(tajTotalCost, 2)}.");
+
+            Console.WriteLine(".................");
+            // Calculating the material cost for the floor plan of the Great Mosque of Mecca.
+            double meccaTotalArea = Rectangles(180, 106) * Rectangles(284, 264) - Triangle(264, 84);
+            double meccaTotalCost = meccaTotalArea * 180;
+            Console.WriteLine(meccaTotalArea);
+            Console.WriteLine($"Your floor area is {meccaTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(meccaTotalCost, 2)}.");
+
         }
         static double Rectangles(double length, double width)
         {
