8aaf7dbd3aab9346f0c424e3d3a2e6f6dbf2963e
georgebid <91198628+georgebid@users.noreply.github.com>
2021-10-15 13:22:19 +0100

    Added 2 more floorplans and gave the user the option to choose which one they want to view.

--- ArchitectArithmetic/Program.cs
+++ ArchitectArithmetic/Program.cs
@@ -8,22 +8,46 @@ namespace ArchitectArithmetic
         {
             double totalArea = Rectangles(2500, 1500) + Triangle(750, 500) + 0.5 * Circles(375);
             double totalCost = totalArea * 180;
-            Console.WriteLine(totalArea);
-            Console.WriteLine($"Your floor area is {totalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be {Math.Round(totalCost, 2)}.");
 
-            Console.WriteLine(".................");
             // Calculating the material cost for the floor plan of Taj Mahal, India.
             double tajTotalArea = Rectangles(90.5, 90.5) - Triangle(24, 24);
             double tajTotalCost = tajTotalArea * 180;
-            Console.WriteLine(tajTotalArea);
-            Console.WriteLine($"Your floor area is {tajTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(tajTotalCost, 2)}.");
 
-            Console.WriteLine(".................");
             // Calculating the material cost for the floor plan of the Great Mosque of Mecca.
             double meccaTotalArea = Rectangles(180, 106) * Rectangles(284, 264) - Triangle(264, 84);
             double meccaTotalCost = meccaTotalArea * 180;
-            Console.WriteLine(meccaTotalArea);
-            Console.WriteLine($"Your floor area is {meccaTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(meccaTotalCost, 2)}.");
+
+            Console.WriteLine("There are 3 floor plans to chose from!");
+            Console.Write("What floor plan would you like?( Teothuacan - 1, Taj Mahal - 2, Mosque of Mecca - 3: ");
+            string choice = Console.ReadLine();
+            string floorPlanChoice = choice.ToUpper();
+
+            switch (floorPlanChoice)
+            {
+                case "1":
+                    {
+                        Console.WriteLine($"Your floor area is {totalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be {Math.Round(totalCost, 2)}.");
+                        break;
+                    }
+
+                case "2":
+                    {
+                        Console.WriteLine($"Your floor area is {tajTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(tajTotalCost, 2)}.");
+                        break;
+                    }
+
+                case "3":
+                    {
+                        Console.WriteLine($"Your floor area is {meccaTotalArea}, and the cost of the flooring material is 180 pesos, therefore the total cost for this building will be ${Math.Round(meccaTotalCost, 2)}.");
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Sorry, we haven't planned that building yet.");
+                        break;
+                    }
+            }
 
         }
         static double Rectangles(double length, double width)
